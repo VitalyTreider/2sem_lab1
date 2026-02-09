8c1859639c4079a19f26fe9647b2e33f195026ce
Vitaly Zatulin <ivanvanusa294@gmail.com>
2026-02-09 12:43:36 +0300

    add files

---
+++ rect.cpp
@@ -0,0 +1,9 @@
+#include "rect.hpp"
+#include <iostream>
+
+Rect::Rect(int c11, int c22, int c33, int c44) {
+    c1 = c11;
+    c2 = c22;
+    c3 = c33;
+    c4 = c44;
+}
---
+++ rect.hpp
@@ -0,0 +1,9 @@
+class Rect {
+    private:
+        int c1;
+        int c2;
+        int c3;
+        int c4;
+    public:
+        Rect(int c11, int c22, int c33, int c44);
+};
